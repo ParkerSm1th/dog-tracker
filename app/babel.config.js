60147190236ca94d4e3e36e409f3d9cb6cf04b32
Parker Smith <me@parkersmith.io>
2024-03-09 19:48:14 -0700

    [DT-14] app: Migrate to nativewind based on v50

--- app/babel.config.js
+++ app/babel.config.js
@@ -5,9 +5,5 @@ module.exports = function(api) {
       ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
       'nativewind/babel',
     ],
-    plugins: [
-      // Required for expo-router
-      'react-native-reanimated/plugin',
-    ],
   };
 };
--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  // NOTE: Update this to include the paths to all of your component files.
   content: ['./app/**/*.{js,jsx,ts,tsx}'],
   presets: [require('nativewind/preset')],
   theme: {
